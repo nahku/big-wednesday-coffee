25c98325c2c26b73f8d9b285b56b04731f6d7d7b
anon <anon@anon.com>
2024-04-29 16:32:12 +0200

    refactoring by extracting methods to separate responsibility

--- src/main/java/com/oocode/Main.java
+++ src/main/java/com/oocode/Main.java
@@ -1,8 +1,11 @@
 package com.oocode;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.StringReader;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -30,17 +33,27 @@ public class Main {
   private static void createPage(String url, LocalDate today, HttpClient httpClient) throws Exception {
     String response = httpClient.readUrl(url);
 
-    List<String[]> result;
-    try (CSVReader reader = new CSVReader(new StringReader(response))) {
-      result = reader.readAll().stream().skip(2).collect(toList());
-    }
-    String[] strings = result.stream().max(comparing(o -> Double.valueOf(o[7]))).orElse(null);
+    String[] output = generateOutput(response);
+
+    writeHtml(output);
+  }
+
+  private static void writeHtml(String[] strings) throws IOException {
     try (FileWriter myWriter = new FileWriter("index.html")) {
       LocalDateTime foo = LocalDateTime.ofEpochSecond(parseInt(strings[2]), 0, ZoneOffset.ofHours(10));
       myWriter.write(
           String.format("<html><body>You should have been at %s on %s - it was gnarly - waves up to %sm!</body></html>",
               strings[0], foo.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH), strings[7]));
     }
+  }
+
+  private static String [] generateOutput(String response) throws IOException, CsvException {
+    List<String[]> result;
+    try (CSVReader reader = new CSVReader(new StringReader(response))) {
+      result = reader.readAll().stream().skip(2).collect(toList());
+    }
+    String[] strings = result.stream().max(comparing(o -> Double.valueOf(o[7]))).orElse(null);
+    return strings;
   }
 }
 
